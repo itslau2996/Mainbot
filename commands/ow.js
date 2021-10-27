33f095c8c428d417e1111a502b17e16e8406c46e
itslau2996 <wezellaurens@gmail.com>
2021-10-27 16:17:43 +0200

    cooldown

--- commands/ow.js
+++ commands/ow.js
@@ -1,12 +1,13 @@
+const talkedRecently = new Set();
 module.exports = {
     name: 'ow',
     description: 'this is a ping command!',
     execute(message, args){
-        const messages = ["#1 Welk seizoen vind jij het leukst?", "#2 Wat is jouw favoriete LOG seizoen?", "#3 Wat is jouw favoriete tijd van je leven?", "#4 Wat doe jij het liefst 'savonds?", "#5 Hoe zou jij je kind noemen en waarom?",
+        const messages = ["#1 Welk seizoen vind jij het leukst?", "#2 Wat is jouw favoriete LOG seizoen?", "#3 Wat is jouw favoriete tijd van je leven?", "#4 Wat doe jij het liefst 's avonds?", "#5 Hoe zou jij je kind noemen en waarom?",
     "#6 Wat zou jij graag nog een keer doen in jouw leven?", "#7 Welk land wil jij nog heel graag bezoeken?", "#8 Welk vak haat je het meest en waarom?"
     , "#9 Hoe vaak ben je nog na 01.00 uur wakker?", "Wat beschouw je als de grofste gewoonte die sommige mensen hebben?", "Ben je georganiseerd in je privéleven of aardig rommelig?",
     "#10 Ben je wel eens bang in het donker?", "#11 Wat staat bovenaan je bucketlist?", "#12 Wat waren je favoriete televisieshows toen je jong was?", "#13 Ben je ooit in echt gevaar geweest?", "#15 Wat zou je liever hebben; een droomauto of een geweldig huis?", 
-    "#16 Wat is je “guilty pleasure” als je moet ontspannen?",  "#17 Wie is de persoon in je leven die je het gelukkigst maakt?", "#18 Als je gestrest bent, wat doe je dan om te ontspannen?",  
+    "#16 Wat is je “guilty pleasure” als je kan ontspannen?",  "#17 Wie is de persoon in je leven die je het gelukkigst maakt?", "#18 Als je gestrest bent, wat doe je dan om te ontspannen?",  
     "#19 Als je een nieuwe voornaam zou mogen kiezen, wat zou het dan worden?",  "#20 Als je een periode in je leven opnieuw zou kunnen doen, wat kies je dan? #21 Waar breng je gedurende de dag de meeste tijd door?", 
     "#22 Wat is de gekste droom die je ooit hebt gehad?", "#22 Is er iets gebeurd in je leven dat niet te verklaren valt?", "#23 Wie heeft een belangrijke rol gespeeld in hoe jij nu bent?", 
     "#24 Wat is de mooiste plek die je ooit hebt bezocht?",  "#25 Waar ben je geweest op je laatste vakantie?",  "#26 Wat vind je leuk aan je werk/school?", 
@@ -43,8 +44,21 @@ module.exports = {
     "#69 *nice* Wat voor een huis wil je later bezitten?",
     "#70 Waar ben je het meest trots op in je leven?",
     "#71 Zijn je doelen en prioriteiten veranderd naarmate je ouder bent geworden?"]
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("calm down, wacht nog 10 minuten met dit command uitvoeren- " + `${message.author}`);
+        } else {
+            const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+            message.channel.send(randomMessage)  
+       // the user can type the command ... your command code goes here :)
 
-        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-        message.channel.send(randomMessage)  
+    // Adds the user to the set so that they can't talk for a minute
+    talkedRecently.add(message.author.id);
+    setTimeout(() => {
+      // Removes the user from the set after 10 minutes
+      talkedRecently.delete(message.author.id);
+    }, 600000);
+}
+
+       
     }
 }
